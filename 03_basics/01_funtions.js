ff433be9cc6c81bda88c56ceaf6d000c8c1bcd22
adityathakur17 <119922740+adityathakur17@users.noreply.github.com>
2024-07-19 09:12:47 +0000

    learned functions and handling objs in functions

---
+++ 03_basics/01_funtions.js
@@ -0,0 +1,19 @@
+function calculateCartPrice(...num1){
+  return(num1);
+}
+
+console.log(calculateCartPrice(299,3444,33,22,2,223,34))
+
+const user = {
+    name: "Aditya",
+    id : "23334342432"
+}
+
+function userAccept(anyobject){
+    console.log(`Hello user ${anyobject.name} your id is ${anyobject.id}`)
+}
+
+userAccept({
+    name:"Adi",
+    id: "2456474563"
+})
